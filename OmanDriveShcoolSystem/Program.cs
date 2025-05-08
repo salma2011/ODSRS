94275059f52483b2950547b6d7380f2daa55007b
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-08 19:55:21 +0400

    Update new things

--- OmanDriveShcoolSystem/Program.cs
+++ OmanDriveShcoolSystem/Program.cs
@@ -4,6 +4,11 @@ public class Program
 {
     static void Main(string[] args)
     {
+
+
+        Console.WriteLine("===================================================");
+        Console.WriteLine("Nizwa Driving School System");
+        Console.WriteLine("===================================================\n");
         List<Student> students = new List<Student>();
         int studentCounter = 1;
 
@@ -12,8 +17,6 @@ public class Program
 
 
         List<BookingLesson> bookingLessons = new List<BookingLesson>();
-
-        Console.WriteLine("Welcome to Oman Driving School Registration System !!");
         while (true)
         {
 
@@ -21,38 +24,39 @@ public class Program
             Console.WriteLine("2 . Register New Instructor");
             Console.WriteLine("3 . Book Driving Lesson");
             Console.WriteLine("4 . Record Lesson Completion & Instructor Feedback");
-            Console.WriteLine("5 . View Student Lesson");
+            Console.WriteLine("5 . View Student Lesson History");
             Console.WriteLine("6 . View All Scheduled Lessons");
-            Console.WriteLine("7 . Exit!!");
+            Console.WriteLine("7 . Exit!");
 
+            Console.Write("\nSelect an Option : ");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
                 case 1:
-                    Console.WriteLine("Select an Option : 1");
+                    Console.WriteLine("\n-- Register New Student --");
                     Student.addStudent(students, ref studentCounter);
                     break;
                 case 2:
-                    Console.WriteLine("Select an Option : 2");
+
 
                     break;
                 case 3:
-                    Console.WriteLine("Select an Option : 3");
+
                     BookingLesson bookingLesson = new BookingLesson();
                     bookingLesson.BookDrivingLesson(students, instructor, "", "", "", "");
                     break;
                 case 4:
-                    Console.WriteLine("Select an Option : 4");
+
 
                     break;
                 case 5:
-                    Console.WriteLine("Select an Option : 5");
+
                     break;
                 case 6:
-                    Console.WriteLine("Select an Option : 6");
+
                     break;
                 case 7:
-                    Console.WriteLine("Select an Option : 7");
+
                     break;
                 default:
 
