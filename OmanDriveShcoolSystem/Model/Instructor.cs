2e7b5108eba84f4dcdc3cb6261bf7dceec1f8f45
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-08 14:57:59 +0400

    addBookDrivingLesson

---
+++ OmanDriveShcoolSystem/Model/Instructor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmanDriving
+{
+   
+    
+
+
+
+    }
+internal class Instructor
+{
+    public enum SpecializationTyp { Light, Heavy, Motocycle }
+
+
+    public string InstructorID { get; set; }
+
+    public string Name { get; set; }
+    public int PhoneNum { get; set; }
+    public string specialization { get; set; }
+
+    public Instructor(string InstructorID, string Name, int PhoneNum, string specializatin)
+    {
+        this.InstructorID = InstructorID;
+        this.Name = Name;
+        this.PhoneNum = PhoneNum;
+        this.specialization = specializatin;
+    }
+
+
+
+    public static void addInstroctur ( List<Instructor> instructors, ref int instruccounter)
+    {
+        Console.WriteLine("Enter number of Instructor : ");
+        int numberOfInstructor = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int i = 0; i < numberOfInstructor; i++)
+        {
+            string indexing = $"Ins{instruccounter++}";
+            Console.WriteLine("Enter Instructor Name: ");
+            string InstName = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number: ");
+            int InstPhone = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Specialization Type (Light / Heavy / Motorcycle): ");
+            string InstSpec = Console.ReadLine();
+            Instructor.SpecializationTyp Insp = (Instructor.SpecializationTyp)Enum.Parse(typeof(Instructor.SpecializationTyp), InstSpec, true);
+            instructors.Add(new Instructor(indexing, InstName, InstPhone, InstSpec));
+            Console.WriteLine("Done Added Instructor ");
+        }
+  foreach(var instructor in instructors) {
+            Console.WriteLine($"ID: {instructor.InstructorID}, Name: {instructor.Name}, Phone: {instructor.PhoneNum}, Specialization: {instructor.specialization}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
--- OmanDriveShcoolSystem/Program.cs
+++ OmanDriveShcoolSystem/Program.cs
@@ -1,57 +1,63 @@
 ﻿using OmanDriveShcoolSystem.Model;
 
-namespace OmanDriveShcoolSystem
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        // test push
-        static void Main(string[] args)
+        List<Student> students = new List<Student>();
+        int studentCounter = 1;
+
+        List<Instructor> instructor = new List<Instructor>();
+        int instruccounter = 1;
+
+
+        List<BookingLesson> bookingLessons = new List<BookingLesson>();
+
+        Console.WriteLine("Welcome to Oman Driving School Registration System !!");
+        while (true)
         {
-            List<Student> students = new List<Student>();
-            int studentCounter = 1;
 
-            Console.WriteLine("Welcome to Oman Driving School Registration System !!");
-            while (true)
+            Console.WriteLine("1 . Register New Student");
+            Console.WriteLine("2 . Register New Instructor");
+            Console.WriteLine("3 . Book Driving Lesson");
+            Console.WriteLine("4 . Record Lesson Completion & Instructor Feedback");
+            Console.WriteLine("5 . View Student Lesson");
+            Console.WriteLine("6 . View All Scheduled Lessons");
+            Console.WriteLine("7 . Exit!!");
+
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
             {
+                case 1:
+                    Console.WriteLine("Select an Option : 1");
+                    Student.addStudent(students, ref studentCounter);
+                    break;
+                case 2:
+                    Console.WriteLine("Select an Option : 2");
 
-                Console.WriteLine("1 . Register New Student");
-                Console.WriteLine("2 . Register New Instructor");
-                Console.WriteLine("3 . Book Driving Lesson");
-                Console.WriteLine("4 . Record Lesson Completion & Instructor Feedback");
-                Console.WriteLine("5 . View Student Lesson");
-                Console.WriteLine("6 . View All Scheduled Lessons");
-                Console.WriteLine("7 . Exit!!");
+                    break;
+                case 3:
+                    Console.WriteLine("Select an Option : 3");
+                    BookingLesson bookingLesson = new BookingLesson();
+                    bookingLesson.BookDrivingLesson(students, instructor, "", "", "", "");
+                    break;
+                case 4:
+                    Console.WriteLine("Select an Option : 4");
 
-                int options = Convert.ToInt32(Console.ReadLine());
-                switch (options)
-                {
-                    case 1:
-                        Console.WriteLine("Select an Option : 1");
-                        Student.addStudent(students, ref studentCounter);
-                        break;
-                    case 2:
-                        Console.WriteLine("Select an Option : 2");
-                        break;
-                    case 3:
-                        Console.WriteLine("Select an Option : 3");
-                        break;
-                    case 4:
-                        Console.WriteLine("Select an Option : 4");
-                        break;
-                    case 5:
-                        Console.WriteLine("Select an Option : 5");
-                        break;
-                    case 6:
-                        Console.WriteLine("Select an Option : 6");
-                        break;
-                    case 7:
-                        Console.WriteLine("Select an Option : 7");
-                        break;
-                    default:
+                    break;
+                case 5:
+                    Console.WriteLine("Select an Option : 5");
+                    break;
+                case 6:
+                    Console.WriteLine("Select an Option : 6");
+                    break;
+                case 7:
+                    Console.WriteLine("Select an Option : 7");
+                    break;
+                default:
 
-                        break;
+                    break;
 
-                }
             }
         }
     }
